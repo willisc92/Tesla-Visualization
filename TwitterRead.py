dee47e1fa4823f2c1026b34f610c5059fef38bd3
Willis Cheung <willis.cheung88@gmail.com>
2019-06-11 16:41:29 -0600

    Fixed json output to be valid json.  Beginning visualizations

--- TwitterRead.py
+++ TwitterRead.py
@@ -2,14 +2,15 @@ import json
 import sys
 import time
 import os
+import pandas as pd 
 
 class FilteredTweet:  # Class to store the clean Tweet data.
     def __init__(self, tweet_dict):
         self.user_ID = tweet_dict['user']['id_str']
         self.screen_name = tweet_dict['user']['screen_name']
-        self.place_country = tweet_dict['place']['country']
-        self.place_longitude = float(tweet_dict['place']['bounding_box']['coordinates'][0][0][0])
-        self.place_latitude = float(tweet_dict['place']['bounding_box']['coordinates'][0][0][1])
+        self.country = tweet_dict['place']['country']
+        self.longitude = float(tweet_dict['place']['bounding_box']['coordinates'][0][0][0])
+        self.latitude = float(tweet_dict['place']['bounding_box']['coordinates'][0][0][1])
         self.sentiment = tweet_dict['sentiment']
         self.text = tweet_dict['text']
         self.timestamp = tweet_dict['timestamp_ms']
@@ -73,31 +74,41 @@ num_eval = 0
 path_to_json = "G:\\Users\\Willis\\Documents\\MENG_Software\\ENEL 645\\Visualization"
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 output_filename = "TESLA_CLEAN_TWEETS.json"  # Name of clean .json file to write to.  Will have 1 clean tweet per line of file.
+list_clean_tweets = []
 
 print("Beginning processing...")
 for js in json_files:
     with open(os.path.join(path_to_json, js), 'r', encoding='utf8', errors='ignore') as f:  # Open input file
         with open(output_filename, 'a', encoding='utf8') as outputFile:  # Open output file
             for (i, line) in enumerate(f, 1):  # Iterate through each line in input file.
-                tweet_dict = json.loads(line)  # Load the .json object into a dictionary.
-                num_eval += 1
                 try:
+                    tweet_dict = json.loads(line)  # Load the .json object into a dictionary.
+                    num_eval += 1
                     if filterTweet(tweet_dict):  # Filter the tweet.
                         new_tweet = FilteredTweet(tweet_dict)  # Create a new filtered tweet.
                         new_tweet.setIndex(index_clean)  # Set the index.
-                        new_tweet_json = json.dumps(
-                            new_tweet.__dict__)  # Create a new json object from the clean tweet.
-                        clean_tweet_json = json.dumps(new_tweet.__dict__, indent=4)
-                        print(clean_tweet_json)
-                        outputFile.write(new_tweet_json)  # Write the new json object to the output file.
-                        outputFile.write('\n')  # Write a newline to separate tweets in output file.
-                        print("Current tweets written from file:", index_clean, ", Total Tweets evaluated from file:",
-                              i, ", Total Tweets read overall:", num_eval)
+                        
+                        new_tweet_dict = new_tweet.__dict__
+                        list_clean_tweets.append(new_tweet_dict)
+
+                        # new_tweet_json = json.dumps(
+                            # new_tweet.__dict__)  # Create a new json object from the clean tweet.
+
+                        # clean_tweet_json = json.dumps(new_tweet.__dict__, indent=4)
+
+                        # print(clean_tweet_json)
+                        # outputFile.write(new_tweet_json)  # Write the new json object to the output file.
+                        # outputFile.write('\n')  # Write a newline to separate tweets in output file.
+                        # print("Current tweets written from file:", index_clean, ", Total Tweets evaluated from file:",
+                        #       i, ", Total Tweets read overall:", num_eval)
                         index_clean += 1
                 except Exception as e:
                     print("Encountered error in line:", i)
                     print(e)
                     continue
+
+tweet_dataframe = pd.DataFrame(list_clean_tweets)
+tweet_dataframe.to_json('TESLA_CLEAN_TWEETS_SINGLE_OBJ.json', orient='records')
 
 print("****************************************")
 print("Total clean tweets written from file: ", index_clean - 1)
